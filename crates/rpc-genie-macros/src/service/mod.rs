e356484d4c39770c2e109c64ff8cbf8ac50cf0e3
Vincent Fries <github.herd7@aleeas.com>
2026-06-01 21:07:58 +0200

    feat: Generated sub services structs

--- crates/rpc-genie-macros/src/service/mod.rs
+++ crates/rpc-genie-macros/src/service/mod.rs
@@ -63,10 +63,14 @@ impl Service {
     fn content(&self) -> TokenStream {
         let server_and_clients_structs = self.server_and_clients_structs();
 
+        let sub_services_structs = self.sub_services_structs();
+
         let rest = &self.rest;
 
         quote! {
             #server_and_clients_structs
+
+            #sub_services_structs
 
             #(#rest)*
         }
@@ -114,6 +118,46 @@ impl Service {
         quote! {
             #server_state
             #client_state
+        }
+    }
+
+    /// Creates the ServerSubServices and ClientSubServices structs
+    fn sub_services_structs(&self) -> TokenStream {
+        let fields_ast_creator = |field_type| {
+            if self.sub_services.is_empty() {
+                // To simplify the code we use empty structs when no sub services are present.
+                // We use the PhantomData to avoid an error due to 'state not being used
+                return quote!(_state_lifetime: std::marker::PhantomData<&'state ()>,);
+            }
+
+            self.sub_services.iter().fold(
+                TokenStream::new(),
+                |acc,
+                 SubService {
+                     pub_keyword,
+                     name,
+                     colon,
+                     path,
+                 }| {
+                    quote! {
+                        #acc
+                        #pub_keyword #name #colon #path :: #field_type,
+                    }
+                },
+            )
+        };
+
+        let server_side_fields = fields_ast_creator(quote!(ServerRequestHandler<'state>));
+        let client_side_fields = fields_ast_creator(quote!(ClientRequestHandler<'state>));
+
+        quote! {
+            pub struct ServerSubServices<'state> {
+                #server_side_fields
+            }
+
+            pub struct ClientSubServices<'state> {
+                #client_side_fields
+            }
         }
     }
 }
