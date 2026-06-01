82c35d090f6bf07de7473337a945b4caeb51c033
Vincent Fries <github.herd7@aleeas.com>
2026-06-01 20:24:27 +0200

    refactor: removed unused create_result helper function

--- crates/rpc-genie-macros/src/service/service_parser/error_helpers.rs
+++ crates/rpc-genie-macros/src/service/service_parser/error_helpers.rs
@@ -4,10 +4,3 @@ pub fn combine_errors(errors: &mut Option<syn::Error>, error: syn::Error) {
         None => *errors = Some(error),
     }
 }
-
-pub fn create_result<T>(ok: T, maybe_errors: Option<syn::Error>) -> syn::Result<T> {
-    match maybe_errors {
-        Some(err) => Err(err),
-        None => Ok(ok),
-    }
-}
