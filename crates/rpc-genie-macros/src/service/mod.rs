26e84063ea3adf106cfb3787b46d92d636d247bf
Vincent Fries <github.herd7@aleeas.com>
2026-06-11 12:43:29 +0200

    refactor: Removed unused use statements and some #[allow(dead_code)]

--- crates/rpc-genie-macros/src/service/mod.rs
+++ crates/rpc-genie-macros/src/service/mod.rs
@@ -1,12 +1,8 @@
 mod service_parser;
 mod code_generation;
 
-use proc_macro2::TokenStream;
-use quote::{ToTokens, TokenStreamExt, quote};
 use syn::{Ident, Item, ItemMod, ItemStruct, PatType, Path, Receiver, Token};
 
-// TODO remove allow(dead_code)
-#[allow(dead_code)]
 pub struct Service {
     module: ItemMod,
     server: ItemStruct,
@@ -17,16 +13,12 @@ pub struct Service {
     rest: Vec<Item>,
 }
 
-// TODO remove allow(dead_code)
-#[allow(dead_code)]
 struct RemoteMethod {
     ident: Ident,
     receiver: Option<Receiver>,
     args: Vec<PatType>,
 }
 
-// TODO remove allow(dead_code)
-#[allow(dead_code)]
 struct SubService {
     pub_keyword: Option<Token![pub]>,
     name: Ident,
