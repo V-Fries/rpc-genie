df44b93998fdd587defa1188be80d7ee51930876
Vincent Fries <github.herd7@aleeas.com>
2026-06-23 16:34:55 +0200

    refactor(code-generation:request_handlers): created new files to separate the code cleanly

---
+++ crates/rpc-genie-macros/src/service/code_generation/request_handlers/impl_handle_request_for_request_handler.rs
@@ -0,0 +1,89 @@
+use proc_macro2::TokenStream;
+use quote::quote;
+
+use crate::service::{RemoteMethod, code_generation::utils::ident_to_lit_str};
+
+pub fn impl_handle_request_for_request_handler(
+    request_handler_struct_name: &TokenStream,
+    associated_state_struct_name: &TokenStream,
+    associated_remote_methods: &[RemoteMethod],
+    opposite_stub_struct_name: &TokenStream,
+) -> TokenStream {
+    let fn_content = fn_content(associated_state_struct_name, associated_remote_methods);
+
+    quote! {
+        impl<'state> rpc_genie::HandleRequest<#opposite_stub_struct_name>
+            for #request_handler_struct_name<'state>
+        {
+            async fn handle_request(
+                &self,
+                method_path: &str,
+                __rpc_client__: #opposite_stub_struct_name,
+                mut __rpc_args__: rpc_genie::Args,
+            ) -> rpc_genie::Result<rpc_genie::ReturnValue> {
+                #fn_content
+            }
+        }
+    }
+}
+
+fn fn_content(
+    associated_state_struct_name: &TokenStream,
+    remote_methods: &[RemoteMethod],
+) -> TokenStream {
+    let sub_services_handle_request_call = quote! {
+        self.sub_services
+            .handle_request(method_path, __rpc_client__, __rpc_args__)
+            .await
+    };
+
+    if remote_methods.is_empty() {
+        return sub_services_handle_request_call;
+    }
+
+    let methods_matches = remote_methods
+        .iter()
+        .map(|remote_method| match_branch(associated_state_struct_name, remote_method));
+
+    quote! {
+        match method_path {
+            #(#methods_matches)*
+            _ => {
+                #sub_services_handle_request_call
+            }
+        }
+    }
+}
+
+fn match_branch(
+    associated_state_struct_name: &TokenStream,
+    remote_method: &RemoteMethod,
+) -> TokenStream {
+    let method_name_as_literal_str = ident_to_lit_str(&remote_method.ident);
+
+    let code_that_parses_args_into_vars = remote_method.args.iter().map(|arg| {
+        let pat = &arg.pat;
+        let ty = &arg.ty;
+        quote!(let #pat = __rpc_args__.read_arg::<#ty>()?;)
+    });
+
+    let args_names = remote_method.args.iter().map(|arg| {
+        let pat = &arg.pat;
+        quote!(#pat)
+    });
+
+    let remote_method_name = &remote_method.ident;
+
+    let method_caller = match remote_method.receiver {
+        None => quote!(#associated_state_struct_name::#remote_method_name(#(#args_names,)*)),
+        Some(_) => quote!(self.state.#remote_method_name(#(#args_names,)*)),
+    };
+
+    quote! {
+        #method_name_as_literal_str => {
+            #(#code_that_parses_args_into_vars)*
+            let res = #method_caller;
+            Ok(rpc_genie::ReturnValue::new(res))
+        }
+    }
+}
