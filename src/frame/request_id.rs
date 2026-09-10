96d9c380cd4019534244e9c5ffb74d409927a2d1
Vincent Fries <github.herd7@aleeas.com>
2026-09-10 18:05:09 +0200

    feat: RequestId now implements Eq, Hash, and Display

--- src/frame/request_id.rs
+++ src/frame/request_id.rs
@@ -1,5 +1,13 @@
-#[derive(Debug, serde::Deserialize, serde::Serialize, Clone, Copy)]
+use std::fmt::Display;
+
+#[derive(Debug, serde::Deserialize, serde::Serialize, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct RpcRequestId(pub u64);
+
+impl Display for RpcRequestId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
 
 impl From<u64> for RpcRequestId {
     fn from(value: u64) -> Self {
