1606c0f3ea37eccbfc3828dc5a8008e6fc676a63
Vincent Fries <github.herd7@aleeas.com>
2026-09-19 22:20:05 +0200

    doc: Added doc to the service-macro sub-crate

--- service-macro/src/lib.rs
+++ service-macro/src/lib.rs
@@ -1,3 +1,9 @@
+//! Macros used by the [`rpc-genie`](https://docs.rs/rpc-genie/latest/rpc-genie/) crate
+//!
+//! It is unlikely you would ever want to use this crate directly. See
+//! [`rpc-genie`](https://docs.rs/rpc-genie/latest/rpc-genie/) for a crate that facilitates with
+//! RPC requests.
+
 use proc_macro::TokenStream;
 use quote::quote;
 
@@ -5,6 +11,7 @@ use crate::service::Service;
 
 mod service;
 
+/// Documentation can be found [here](https://docs.rs/rpc-genie/latest/rpc-genie/attr.service.html)
 #[proc_macro_attribute]
 pub fn service(_attr: TokenStream, tokens: TokenStream) -> TokenStream {
     let service = syn::parse_macro_input!(tokens as Service);
